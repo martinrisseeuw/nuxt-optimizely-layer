7d7139d2d379b8f8d346a0a96a887f51446506f0
martinrisseeuw <hello@martinr.nl>
2025-03-19 16:17:40 +0100

    feat: initial commit

--- plugins/optimizely.client.ts
+++ plugins/optimizely.client.ts
@@ -1,41 +1,10 @@
-// import optimizely from '@optimizely/optimizely-sdk';
-
-// export default defineNuxtPlugin({
-//   name: 'optimizely',
-//   enforce: 'post',
-//   async setup(nuxtApp) {
-//     const sdkKey = useRuntimeConfig().public.optimizelySdkKey;
-//     const DATAFILE_URL = `https://cdn.optimizely.com/datafiles/${sdkKey}.json`;
-//     const datafile = await $fetch<object>(DATAFILE_URL);
-//     const optimizelyClient = optimizely.createInstance({ datafile });
-
-//     return {
-//       provide: {
-//         optimizely: optimizelyClient,
-//       },
-//     }
-//   }
-// })
-
-
 import optimizely from '@optimizely/optimizely-sdk'
-import { getOptimizelyClient } from '@/server/utils/optimizely'
-import type {Client} from '@optimizely/optimizely-sdk'
 
-export default defineNuxtPlugin(async (nuxtApp) => {
-  let optimizelyClient: Client | null = null
-
-  if (import.meta.server) {
-    // Use server instance when running SSR
-    optimizelyClient = await getOptimizelyClient()
-  } else {
-    // Fetch datafile for client-side usage
-    const sdkKey = useRuntimeConfig().public.optimizelySdkKey;
-    const DATAFILE_URL = `https://cdn.optimizely.com/datafiles/${sdkKey}.json`;
-    const datafile = await $fetch<object>(DATAFILE_URL);
-    
-    optimizelyClient = optimizely.createInstance({ datafile });
-  }
+export default defineNuxtPlugin(async () => {
+  const sdkKey = useRuntimeConfig().public.optimizelySdkKey;
+  const DATAFILE_URL = `https://cdn.optimizely.com/datafiles/${sdkKey}.json`;
+  const datafile = await $fetch<object>(DATAFILE_URL);
+  const optimizelyClient = optimizely.createInstance({ datafile });
 
   return {
     provide: {
--- plugins/optimizely.server.ts
+++ plugins/optimizely.server.ts
@@ -1,6 +1,6 @@
 import { getOptimizelyClient } from '@/server/utils/optimizely'
 
-export default defineNuxtPlugin(async (nuxtApp) => {
+export default defineNuxtPlugin(async () => {
   const optimizelyClient = await getOptimizelyClient()
 
   return {
